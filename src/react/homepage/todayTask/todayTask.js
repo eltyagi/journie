f9346798b3185bc6d0c951f442729ad4d6b03d15
Lakshya3190 <lakshya_tyagi@icloud.com>
2021-01-12 10:49:07 +0530

    Enabled express API connection

--- src/react/homepage/todayTask/todayTask.js
+++ src/react/homepage/todayTask/todayTask.js
@@ -7,14 +7,44 @@ class todayTask extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            addTaskVisibility: false
+            addTaskVisibility: false,
+            taskType: "",
+            taskTitle: "",
+            taskDesc: ""
         }
 
 
     }
 
+    onTaskTypeChange = (event) => {
+        this.setState({taskType: event.target.value})
+    }
+
+    onTaskTitleChange = (event) => {
+        this.setState({taskTitle: event.target.value})
+    }
+
+    onTaskDesChange = (event) => {
+        this.setState({taskDesc: event.target.value})
+    }
+
     toggleAddTask = () => {
         this.setState({addTaskVisibility: !this.state.addTaskVisibility});
+    }
+
+    onSubmitTask = () => {
+        fetch('http://localhost:3001/', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                taskType: this.state.taskType,
+                taskTitle: this.state.taskTitle,
+                taskDesc: this.state.taskDesc
+            })
+        })
+            .then(response => response.json())
+            .then(console.log("Communication achieved"))
+        
     }
 
     render(){
@@ -25,13 +55,13 @@ class todayTask extends React.Component{
                 </div>
 
                 <div style = {{visibility: this.state.addTaskVisibility ? "visible" : "hidden"}}>
-                    <input type ="text" placeholder = "Task/Event" className = "input-field"></input>
+                    <input onChange = {this.onTaskTypeChange} type ="text" placeholder = "Task/Event" className = "input-field"></input>
                     <br/>
-                    <input type ="text" placeholder = "Title" className = "input-field"></input>
+                    <input onChange = {this.onTaskTitleChange} type ="text" placeholder = "Title" className = "input-field"></input>
                     <br/>
-                    <input type ="text" placeholder = "Description" className = "input-field"></input>
+                    <input onChange = {this.onTaskDesChange} type ="text" placeholder = "Description" className = "input-field"></input>
                     <br/>
-                    <p onClick = {this.toggleAddTask} className = 'add-button pointer'>Add</p>
+                    <p onClick = {() => {this.toggleAddTask(); this.onSubmitTask()}} className = 'add-button pointer'>Add</p>
 
                 </div>
             </div>
